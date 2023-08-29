44fa78abec08da2d2aa7e8bd85071198c59621e2
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-08-29 01:34:34 -0300

    resolução do exercício 8

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Exercicio8 {
 
     public static void main(String[] args) {
 
+        int horarioDeInicio, horarioDeFim, duracaoPartida;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o horário que a partida iniciou: ");
+        horarioDeInicio = input.nextInt();
+
+        System.out.print("Digite o horário que a partida terminou: ");
+        horarioDeFim = input.nextInt();
+
+        duracaoPartida = horarioDeFim - horarioDeInicio;
+
+        if (duracaoPartida > 24){
+            System.out.println("uma partida não pode durar mais de 24 horas");
+        }else{
+            System.out.print("A partida durou " + duracaoPartida + " horas");
+        }
     }
 }
